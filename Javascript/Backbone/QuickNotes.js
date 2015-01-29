6692e5ef2e651280c4284f80842d49b3c81576d5
jpramirezpra <futbol10>
2015-01-29 17:17:46 -0600

    Changes to QuickNotes for Backbone

--- Javascript/Backbone/QuickNotes.js
+++ Javascript/Backbone/QuickNotes.js
@@ -2,6 +2,9 @@
 var Class = Backbone.Model.extend({
   default: {
     attribute: 'Default Value'
+  },
+  urlRoot: '/restRoot',
+  functionName: function(){
   }
 });
 
@@ -28,3 +31,50 @@ instance.destroy(); //DELTE id
 
 
 //VIEWS
+var View = Backbone.View.extend({
+  template: _.template(),
+  events:{
+    "click" : "alertTitle",
+    "<event> <selector>" : "<method>"
+    //will delegate to inside the View, not bubble out
+  },
+  initialize: function(){
+    this.model.on('change', this.render, this);
+    //need to pass this context, if not would be the window context
+  },
+  alertTitle: function(){
+    alert(this.model.get('title'));
+  },
+  render: function(){
+    this.$el.html(this.template(this.model.toJSON()));
+  },
+  remove: function(){
+    this.$el.remove();
+  }
+})
+
+var viewInstance = new View({model: instance});
+//can call the render function on init, or can call it yourself later
+viewInstance.render();
+
+//Collections
+var CollectionList = Backbone.Collection.extend({
+  url: '/restUrlPlural'
+});
+
+var list = new CollectionList();
+
+list.fetch();
+
+list.length; // = length
+
+list.on('event-name', function(){
+
+});
+
+//events
+//add , remove, reset   on add and remove you can pass in the instance
+
+list.on('add', function(item){
+
+});
